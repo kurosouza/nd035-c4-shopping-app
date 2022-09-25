615e99a68996fde2bfa430634fa4e94caf71d527
Kuro <kurosouza@gmail.com>
2022-09-25 08:41:15 +0100

    Added logging points in controllers.

--- starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,8 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RequestMapping("/api/cart")
 public class CartController {
 	
+	private Logger LOG = LoggerFactory.getLogger(CartController.class);
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -34,6 +38,7 @@ public class CartController {
 	
 	@PostMapping("/addToCart")
 	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+		LOG.debug("Adding item to cart: " + request.toString());
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -51,6 +56,7 @@ public class CartController {
 	
 	@PostMapping("/removeFromCart")
 	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
+		LOG.debug("Removing item from cart: " + request.toString());
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
 import org.hibernate.cfg.CreateKeySecondPass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +23,8 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RequestMapping("/api/user")
 public class UserController {
 	
+	private Logger LOG = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -32,17 +36,20 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		LOG.debug("Called UserController#findUserById, NAME = " + id);
 		return ResponseEntity.of(userRepository.findById(id));
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		LOG.debug("Called UserController#findUserByName, NAME = " + username);
 		User user = userRepository.findByUsername(username);
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		LOG.debug("Creating user: " + createUserRequest.toString());
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
 		Cart cart = new Cart();
@@ -56,6 +63,7 @@ public class UserController {
 		user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
 		
 		userRepository.save(user);
+		LOG.debug("User created: " + user.getUsername());
 		return ResponseEntity.ok(user);
 	}
 	
